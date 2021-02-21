658b19868fe12a77109237438adcdbacce89abec
chaofan-zheng <417355570@qq.com>
2021-02-21 19:14:45 +0800

    update

--- month05/day02_course/day02_code/09_intregrated_exercise.py
+++ month05/day02_course/day02_code/09_intregrated_exercise.py
@@ -1,3 +1,5 @@
+import hashlib
+
 import pymysql
 import pymongo
 import csv
@@ -6,6 +8,18 @@ import re
 import time
 import random
 import redis
+import sys
+
+"""
+瓜子二手车的增量爬虫
+一级页面：连接
+二级页面：汽车名称
+要求：
+    使用redis实现增量
+    使用mysql、mongodb、csv存储结果
+create database guazidb default charset utf8;
+create table guazi (href varchar(100), title varchar(50));
+"""
 
 
 class GuaZiSpider:
@@ -16,7 +30,7 @@ class GuaZiSpider:
             'Cookie': 'track_id=177523292806569984; uuid=6ebc3189-7d01-4c0a-e93e-e23a2a101806; antipas=88B3m44974066S379r05382; clueSourceCode=%2A%2300; ganji_uuid=2127355730094556457784; sessionid=b53b6fc9-334c-4155-9a50-ed617c764890; lg=1; cainfo=%7B%22ca_a%22%3A%22-%22%2C%22ca_b%22%3A%22-%22%2C%22ca_s%22%3A%22pz_baidu%22%2C%22ca_n%22%3A%22pcbiaoti%22%2C%22ca_medium%22%3A%22-%22%2C%22ca_term%22%3A%22-%22%2C%22ca_content%22%3A%22%22%2C%22ca_campaign%22%3A%22%22%2C%22ca_kw%22%3A%22-%22%2C%22ca_i%22%3A%22-%22%2C%22scode%22%3A%22-%22%2C%22keyword%22%3A%22-%22%2C%22ca_keywordid%22%3A%22-%22%2C%22ca_transid%22%3A%22%22%2C%22platform%22%3A%221%22%2C%22version%22%3A1%2C%22track_id%22%3A%22177523292806569984%22%2C%22display_finance_flag%22%3A%22-%22%2C%22client_ab%22%3A%22-%22%2C%22guid%22%3A%226ebc3189-7d01-4c0a-e93e-e23a2a101806%22%2C%22ca_city%22%3A%22hz%22%2C%22sessionid%22%3A%22b53b6fc9-334c-4155-9a50-ed617c764890%22%7D; Hm_lvt_bf3ee5b290ce731c7a4ce7a617256354=1613480054; user_city_id=26; close_finance_popup=2021-02-17; _gl_tracker=%7B%22ca_source%22%3A%22-%22%2C%22ca_name%22%3A%22-%22%2C%22ca_kw%22%3A%22-%22%2C%22ca_id%22%3A%22-%22%2C%22ca_s%22%3A%22self%22%2C%22ca_n%22%3A%22-%22%2C%22ca_i%22%3A%22-%22%2C%22sid%22%3A77702636874%7D; cityDomain=hz; preTime=%7B%22last%22%3A1613572428%2C%22this%22%3A1613480051%2C%22pre%22%3A1613480051%7D; Hm_lpvt_bf3ee5b290ce731c7a4ce7a617256354=1613572430'
         }
         # mysql 连接
-        self.mysql_db = pymysql.connect('localhost', 'root', '417355570', 'noveldb', charset='utf8')
+        self.mysql_db = pymysql.connect('localhost', 'root', '417355570', 'guazidb', charset='utf8')
         self.cur = self.mysql_db.cursor()
 
         # mongoDB连接
@@ -45,9 +59,14 @@ class GuaZiSpider:
         rlist = self.refunc(first_regex, first_html)
         for r in rlist:
             item = {}
-            item['href'] = 'https://www.guazi.com' + r.strip()
-            print(item['href'])
-            self.get_second_data(item['href'], item)
+            item['href'] = 'https://www.guazi.com' + r.strip()  # 注意这里的细节，斜杠要不要写
+            # print(item['href'])
+            finger = self.hash_gen(item)
+            res = self.r.sadd('guazi', finger)
+            if res == 1:
+                self.get_second_data(item['href'], item)
+            else:
+                sys.exit('更新完成')
 
     def get_second_data(self, second_url, item):
         second_html = self.get_html(second_url)
@@ -58,21 +77,43 @@ class GuaZiSpider:
             second_item['href'] = item['href']
             second_item['title'] = second.strip()
             print(second_item)
+            self.save_mongo(second_item)
+            self.save_mysql(second_item)
+            self.save_csv(second_item)
             time.sleep(random.uniform(0, 2))
 
-    def save_mongo(self):
-        pass
+    def save_mongo(self, item):
+        self.set.insert_one(item)
 
-    def save_mysql(self):
-        pass
+    def save_mysql(self, item):
+        ins = 'insert into guazi values(%s,%s)'
+        self.cur.execute(ins, [item['href'], item['title']])
 
-    def save_csv(self):
-        pass
+    def save_csv(self, item):
+        self.writer.writerow([item['href'], item['title']])
+
+    def hash_gen(self, item):
+        md5 = hashlib.md5()
+        md5.update(item['href'].encode())  # encode 不要忘！！
+        finger = md5.hexdigest()
+        return finger
 
     def crawl(self, pagen):
         for page in range(1, pagen + 1):
             url = self.url.format(page)
             self.parse_html(url)
+        self.close()
+
+    def close(self):
+        # mysql
+        self.mysql_db.commit()
+        self.cur.close()
+        self.mysql_db.close()
+        # mongodb
+        # mongodb不需要关闭，会自动维护连接池；不需要提交，没有事务机制
+        # csv
+        self.f.close()
+        # redis
 
 
 if __name__ == '__main__':
---
+++ month05/day03_course/day03_code/test.py
@@ -0,0 +1,3 @@
+str01 = 'This is a test string.'
+print(str01.upper())
+print(str01.title())
