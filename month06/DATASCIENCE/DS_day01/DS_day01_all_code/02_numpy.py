5d000d5979c033c41896b374ca4fdae8b5f19831
chaofan-zheng <417355570@qq.com>
2021-03-03 14:45:07 +0800

    update

---
+++ month06/DATASCIENCE/DS_day01/DS_day01_all_code/02_numpy.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+list01 = [1, 2, 3, 4]
+arr = np.array(list01)
+print(list01)
+print(arr)
+print(type(arr))
+print(arr[0])
+print(list01 + list01)
+print(arr + arr)
+arr2 = np.array([1, 1, 1, 1, 1])
+print(arr + arr2)
---
+++ month06/DATASCIENCE/DS_day01/DS_day01_all_code/pytest.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+np.random.seed(100)
+data1 = np.random.randn(3, 3)
+print(data1)
+data2 = np.random.randint(1,100,[3,5])
+print(data2)
